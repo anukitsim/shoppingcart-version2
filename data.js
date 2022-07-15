2de27cb49e279995343a158e2abf7a2cc6300a3a
anukitsim <104069419+anukitsim@users.noreply.github.com>
2022-07-15 06:05:19 +0400

    ..

---
+++ data.js
@@ -0,0 +1,42 @@
+let shopItemsData = [{
+    id: "accessorie1",
+    price:100,
+    img: "img/BO.png"
+
+
+} , {
+    id: "accessorie2",
+    price:200,
+    img: "img/Claw.png"
+
+}, {
+    id: "accessorie3",
+    price:150,
+    img: "img/Cosmo.png"
+
+}, {
+    id: "accessorie4",
+    price:200,
+    img: "img/Cross2.png"
+
+}, {
+    id: "accessorie5",
+    price:150,
+    img: "img/SOH.png"
+
+}, {
+    id: "accessorie6",
+    price:100,
+    img: "img/tear.png"
+
+}, {
+    id: "accessorie7",
+    price:200,
+    img: "img/V.png"
+
+}, {
+    id: "accessorie8",
+    price:250,
+    img: "img/VU2.png"
+
+}]
