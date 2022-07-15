19899b1f65cd8d7b9b923ae47848eff000bdc9d3
anukitsim <104069419+anukitsim@users.noreply.github.com>
2022-07-15 05:54:00 +0400

    ..

--- script.js
+++ script.js
@@ -44,12 +44,13 @@ let shopItemsData = [{
 
 }]
 
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 
 let generatiShop = () => {
     return  (shop.innerHTML = shopItemsData.map((x) => {
         let {id, price, img} = x;
+        let search = basket.find((x)=> x.id === id) || []
         return `<div id=product-id-${id} class="item">
         <img src=${img} alt="no Ring found">
         <div class="details">
@@ -57,7 +58,7 @@ let generatiShop = () => {
                 <h2>$ ${price}</h2>
                 <div class="buttons">
                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity">0</div>
+                    <div id=${id} class="quantity">${search.item === undefined? 0: search.item}</div>
                     <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
@@ -87,6 +88,7 @@ let increment = (id) => {
     else{
         search.item +=1;
     }
+    localStorage.setItem("data", JSON.stringify(basket));
   
    
     update(selectItem.id);
@@ -95,19 +97,36 @@ let increment = (id) => {
 
 let decrement = (id) => {
     let selectItem = id;
-    let search = basket.find((x) => x.id === selectItem.id )
+    let search = basket.find((x) => x.id === selectItem.id );
 
-    if(search.item === 0) return;
+    if(search === undefined) return;
+
+
+
+    else if(search.item === 0) return;
     else{
         search.item -=1;
     }
-  
-    
     update(selectItem.id);
+    basket = basket.filter((x)=> x.itrm !== 0 );
+    
+  
+
+    localStorage.setItem("data", JSON.stringify(basket));
 }
+
+
 
 let update = (id) => {
     let search = basket.find((x)=> x.id === id)
-    console.log(search.item);
+    
     document.getElementById(id).innerHTML = search.item;
+    calculate();
 }
+
+let calculate = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x)=> x.item).reduce((x,y)=> x+y,0)   //0-default number, calculation should start from 0.
+}
+
+calculate();
